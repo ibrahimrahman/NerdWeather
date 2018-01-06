1d12ce614223817537514398c11f431e85dcda11
jweckschmied <jonas@ib-weckschmied.de>
2018-01-06 12:41:13 +0100

    added custom button, PopupWindow implementation

--- app/src/main/java/com/emiappnw/nerdweather/MainActivity.java
+++ app/src/main/java/com/emiappnw/nerdweather/MainActivity.java
@@ -5,12 +5,19 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.app.AlertDialog.Builder;
 
@@ -18,42 +25,33 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Declare view variables
-        final TextView currentTemp = findViewById(R.id.currentTemp);
-        ImageButton showStatsDialog = findViewById(R.id.showStatsDialog);
-
-        //Do stuff (TEST)
-        currentTemp.setText("TMP C");
-
-        //Switch between C and F
-        currentTemp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               /* TODO 1: Implement unit conversion and display */
-            }
-        });
-
-        //Stats Dialog
-        showStatsDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog statsDialog = new Builder(MainActivity.this)
-                .setView(getLayoutInflater().inflate(R.layout.dialog_temp_stats, null)).create();
-                statsDialog.getWindow().setBackgroundDrawableResource(R.color.transparent);
-                statsDialog.getWindow().setDimAmount(0.0f);
-                statsDialog.show();
-            }
-        });
-
-
     }
+
+        public void onButtonShowPopupWindowClick(View view) {
+
+            // reference main layout
+            ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+
+            // inflate popup window layout
+            LayoutInflater inflater = (LayoutInflater)
+                    getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.dialog_temp_stats, null);
+
+            // create the popup window
+            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true; // lets taps outside the popup also dismiss it
+            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+            // show the popup window
+            popupWindow.showAtLocation(mainLayout, Gravity.CENTER_HORIZONTAL, 0, -300);
+        }
+
 
     //Inject Calligraphy into Context
     @Override
